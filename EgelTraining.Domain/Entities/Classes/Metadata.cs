2894582bc4f77bdd7cea2aaf8596f0bf153d9827
Carlos Ventura <cventura@outlook.com>
2016-03-27 16:26:16 -0700

    validacion de tamaño en Carrera Siglas

---
+++ EgelTraining.Domain/Entities/Classes/Metadata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EgelTraining.Domain.Entities
+{
+    public class CarreraMetadata
+    {
+        [StringLength(4)]
+        public string Siglas { set; get; } 
+    }
+}
---
+++ EgelTraining.Domain/Entities/Classes/PartialClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EgelTraining.Domain.Entities;
+
+namespace EgelTraining.Domain.Entities
+{
+   [MetadataType(typeof(CarreraMetadata))]
+   public partial class Carrera
+   {
+
+   }
+}
