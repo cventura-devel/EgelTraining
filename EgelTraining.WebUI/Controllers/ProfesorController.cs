715e9495124b2cb3d656711b1560d99c67904146
Carlos Ventura <cventura@outlook.com>
2016-03-27 22:13:32 -0700

    added controlls and index

---
+++ EgelTraining.WebUI/Controllers/ProfesorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EgelTraining.Domain.Abstract;
+
+namespace EgelTraining.WebUI.Controllers
+{
+    public class ProfesorController : Controller
+    {
+
+        private IProfesorRepository repository;
+
+
+        public ProfesorController(IProfesorRepository profesorRepository)
+        {
+            repository = profesorRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository);
+        }
+
+    }
+}
